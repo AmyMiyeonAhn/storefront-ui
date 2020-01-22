0938414774735aecad9c0ec50df9edd16aade406
amyahn <amy@loveknitting.com>
2020-01-22 11:22:32 +0000

    extra commite on my branch

--- packages/vue/src/components/atoms/SfBadge/SfBadge.stories.js
+++ packages/vue/src/components/atoms/SfBadge/SfBadge.stories.js
@@ -16,7 +16,14 @@ storiesOf("Atoms|Badge", module)
         default: options(
           "CSS modifiers",
           {
+            /* LC Customised CSS modifiers
             "sf-badge--full-width": "sf-badge--full-width",
+            "sf-badge--card-sale" : "sf-badge--card-sale",
+            "sf-badge--card-new" : "sf-badge--card-new",
+            "sf-badge--card-free" : "sf-badge--card-free",
+            "sf-badge--card-shade" : "sf-badge--card-shade",
+            "sf-badge--small-sale" : "sf-badge--small-sale",
+            "sf-badge--small-new" : "sf-badge--small-new",*/
             "color-primary": "color-primary",
             "color-secondary": "color-secondary",
             "color-warning": "color-warning",
